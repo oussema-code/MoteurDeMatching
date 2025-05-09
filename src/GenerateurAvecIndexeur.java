fefaabac1226327242e99c151abc93e560fef885
oussema ben ameur <oussemabenameur016@gmail.com>
2025-05-09 17:35:07 +0100

    GenerateurDone

--- src/GenerateurAvecIndexeur.java
+++ src/GenerateurAvecIndexeur.java
@@ -1,2 +1,27 @@
-public class GenerateurAvecIndexeur {
+import java.util.*;
+
+public class GenerateurAvecIndexeur implements GenerateurDeCandidats {
+
+    public List<Couple> generer(List<Nom> noms1, List<Nom> noms2) {
+        List<Couple> couples = new ArrayList<>();
+        IndexeurDictionnaire indexeur = new IndexeurDictionnaire();
+        Map<Integer, List<Nom>> index = (Map<Integer, List<Nom>>) indexeur.indexer(noms2);
+
+        for (Nom nom1 : noms1) {
+            int longueur = nom1.getNom().length();
+            double marge = longueur * 0.2;
+            int min = (int) Math.floor(longueur - marge);
+            int max = (int) Math.ceil(longueur + marge);
+
+            for (int key : index.keySet()) {
+                if (key >= min && key <= max) {
+                    for (Nom nom2 : index.get(key)) {
+                        couples.add(new Couple(nom1, nom2));
+                    }
+                }
+            }
+        }
+
+        return couples;
+    }
 }
